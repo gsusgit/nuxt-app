948915ec6a438c4f81bacb0b641f554a24d832d2
Jesús Fernández Jodar <jesus.fernandez@gesthispania.com>
2024-11-27 16:53:44 +0100

    globally page navigation transitions, wow

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
       <main
         class="min-h-screen max-w-5xl mx-auto justify-center items-center text-center mt-8"
       >
-        <NuxtPage class="animate__animated animate__fadeIn" />
+        <NuxtPage />
       </main>
     </NuxtLayout>
   </div>
@@ -14,3 +14,15 @@
 <script setup>
 import AppAlert from '~/components/AppAlert.vue'
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/scss/main.scss', 'animate.css'],
+  css: ['~/assets/scss/main.scss'],
   app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       title: 'My Nuxt App'
     }
